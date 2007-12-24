9b5caa8f5d192fff498ff8592fa90c3b6e6e8942
countdraculla <countdraculla@e07c8e58-f341-0410-88c6-cd3d2a16d4a3>
2007-12-24 05:35:33 +0000

    GMap Fixed

--- trunk/app/views/welcome/gmap.php
+++ trunk/app/views/welcome/gmap.php
@@ -1,14 +1,11 @@
-<?
-$map = $app->library->gmap;
-//$map->setMapType("satellite");
-?>
 <head>
-  <? $map->printGoogleJS(); ?>
+  <? 
+  $map = $app->library->gmap;
+  $map->printGoogleJS(); 
+  ?>
 </head>
 <body>
 <?
-
-
 $map->addGeoPoint(38.517885,-122.18462,"1013 Headlands Dr., Napa, Ca");
 $map->showMap();
 
