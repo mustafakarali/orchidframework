5e7fd52b500a845bdeb5693f4f01220dd7399d11
countdraculla <countdraculla@e07c8e58-f341-0410-88c6-cd3d2a16d4a3>
2007-12-24 07:59:48 +0000

    Improvement in Caching Engine and Drivers

--- trunk/core/main/cache/interface/interface.cache.php
+++ trunk/core/main/cache/interface/interface.cache.php
@@ -3,8 +3,8 @@ interface cachemanager
 {
 	public function setup($cacheinfo);
 	public function get($key);
-	public function set($key, $value, $extra=null);
-	public function isExpired($key, $time=null);
-	public function invalidate($key);
+	public function set($key, $value, $time=86400);
+	public function isExpired($key);
+	public function invalidate($key, $forced=false);
 }
 ?>
