0f56efb0bbabfe2afb98f6fcabbce69e7def0543
countdraculla <countdraculla@e07c8e58-f341-0410-88c6-cd3d2a16d4a3>
2007-12-24 10:07:05 +0000

    Added General Javascript Helper Library

---
+++ trunk/core/js/general.php
@@ -0,0 +1,20 @@
+<?
+header("Content-type: text/javascript; charset: UTF-8");
+?>
+function orchid()
+{
+	this.addFLVPlayer= function(playerurl,url, width, height, container,preview, allowfullscreen)
+	{
+		var s1 = new SWFObject(playerurl,"single",width,height,"7");
+		s1.addParam("allowfullscreen",allowfullscreen);
+		s1.addVariable("file",url);
+		if (preview!="")
+		s1.addVariable("image",preview);
+		s1.addVariable("width",width);
+		s1.addVariable("height",height);
+		s1.write(container);
+
+	}
+}
+
+var orchid = new orchid();
