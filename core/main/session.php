32f334668c4795de8f244d685a72a9293244b131
countdraculla <countdraculla@e07c8e58-f341-0410-88c6-cd3d2a16d4a3>
2008-03-08 17:33:50 +0000

    Added support for easy session clearning without Destroying

--- core/main/session.php
+++ core/main/session.php
@@ -1,10 +1,10 @@
 <?php
 /*
-   CREATE TABLE `sessions` (
-  `id` varchar(32) NOT NULL,
-  `access` int(10) default NULL,
-  `data` text,
-  PRIMARY KEY  (`id`)
+CREATE TABLE `sessions` (
+`id` varchar(32) NOT NULL,
+`access` int(10) default NULL,
+`data` text,
+PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1
 
 */
@@ -19,8 +19,8 @@ class session
 {
 	private $maxTime;
 	private $sessionTable;
-	
-	
+
+
 	public function __get($var)
 	{
 		return $_SESSION[$var];
@@ -36,8 +36,9 @@ class session
 		session_start();
 	}
 
-	public function unsetkey($key)
+	public function unsetKey($key)
 	{
+		$_SESSION[$key]="";
 		unset($_SESSION[$key]);
 	}
 
@@ -46,16 +47,24 @@ class session
 		session_destroy();
 	}
 
+	public function clear()
+	{
+		foreach ($_SESSION as $key=>$val){
+			$_SESSION[$key]="";
+			unset($_SESSION[$key]);
+		}
+	}
+
 
 	public function __construct()
 	{
 		$config = loader::load("config");
-		
+
 		if ($config->use_session_db)
 		{
 			$table = $config->session_table;
 			if (!empty($table)){
-				
+
 				$this->sessionTable = $config->session_table;
 				$this->maxTime = get_cfg_var('session.gc_maxlifetime');
 				session_set_save_handler(
@@ -66,16 +75,16 @@ class session
 				array( $this, "_destroy"),
 				array($this,"_gc")
 				);
-				
+
 			}
 		}
 		$this->start();
 	}
-	
+
 	public function _open() {
 		return true;
 	}
-	
+
 	public function _close()
 	{
 		$time = time();
@@ -83,7 +92,7 @@ class session
 		$db->execute("DELETE FROM {$this->sessionTable} WHERE ($time-access>{$this->maxTime})");
 		return true;
 	}
-	
+
 	public function _write($id, $data)
 	{
 		//die("Writing");
@@ -94,7 +103,7 @@ class session
 		$db->execute("REPLACE INTO {$this->sessionTable} VALUES ('{$id}', '{$time}', '{$data}')");
 		return true;
 	}
-	
+
 	public function _read($id)
 	{
 		$db = loader::load("db");
@@ -104,17 +113,17 @@ class session
 		//$data = unserialize(stripslashes($result['data']));
 		return $result['data'];
 	}
-	
+
 	public function _destroy($id)
 	{
 		$db = loader::load("db");
 		$result = $db->execute("DELETE FROM {$this->sessionTable} WHERE id='{$id}' ");
 		return true;
 	}
-	
+
 	public function _gc()
 	{
-		
+
 	}
 
 
