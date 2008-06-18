27a84c67b433a91336399a1bc8300139b757d898
countdraculla <countdraculla@e07c8e58-f341-0410-88c6-cd3d2a16d4a3>
2008-06-18 18:06:27 +0000

    small fix on layout loader

--- core/libraries/base.php
+++ core/libraries/base.php
@@ -48,12 +48,10 @@ class base{
 
 		if ($uselayout) {
 			$layoutdata = ob_get_clean();
-			$layoutfilelocal = "app/views/{$controller}/{$controller}.php";
+			//$layoutfilelocal = "app/views/{$controller}/{$controller}.php";
 			$layoutfileglobal = "app/views/layouts/{$controller}.php";
 
-			if (file_exists($layoutfilelocal))
-			include_once($layoutfilelocal);
-			else
+			if (file_exists($layoutfileglobal))
 			include_once($layoutfileglobal);
 		}
 	}
