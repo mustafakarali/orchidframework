5b909544ddabd3641df680ca518574a023bb797a
countdraculla <countdraculla@e07c8e58-f341-0410-88c6-cd3d2a16d4a3>
2008-07-05 10:03:26 +0000

    "Documentation of Controller"

--- trunk/app/models/postmodel.php
+++ trunk/app/models/postmodel.php
@@ -4,13 +4,6 @@ class postmodel extends activemodel
 	public $name;
 	public $roll;
 	
-	public $hasmany = array(
-			"comments"=>array(
-				"rule"=>"comments.post_id=posts.id"
-			)
-	);
-	
-	
 	public function getPost()
 	{
 		$router = loader::load("router");
